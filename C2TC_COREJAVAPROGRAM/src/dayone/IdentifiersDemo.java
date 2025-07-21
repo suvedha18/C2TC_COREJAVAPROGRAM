d5a3ed1a4d08b3a62b2db50ba2a832fbaa63e7c0
HP <HP@LAPTOP-463TH5EG>
2025-07-21 23:26:32 +0530

    This is my day1 assessment

---
+++ C2TC_COREJAVAPROGRAM/src/dayone/IdentifiersDemo.java
@@ -0,0 +1,15 @@
+package dayone;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; //use camelcase
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
