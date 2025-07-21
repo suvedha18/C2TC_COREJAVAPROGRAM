cfa9340e2a8cf0aa4f7a67366b5274ba760d0a73
HP <HP@LAPTOP-463TH5EG>
2025-07-21 16:36:37 +0530

    customer project

---
+++ C2TC_COREJAVAPROGRAM/src/day1/basics/Addition2.java
@@ -0,0 +1,19 @@
+package day1.basics;
+import java.util.*;
+
+public class Addition2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the values");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("addition of c="+a+" and b="+b+" is"+c);
+		
+
+	}
+
+}
