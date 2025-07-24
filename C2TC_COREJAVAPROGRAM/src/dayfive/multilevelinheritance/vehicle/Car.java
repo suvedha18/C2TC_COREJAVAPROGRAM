e6136c650f52e6bfe56a192cc3a353ba54caff80
HP <HP@LAPTOP-463TH5EG>
2025-07-24 21:09:34 +0530

    This is Day five package

---
+++ C2TC_COREJAVAPROGRAM/src/dayfive/multilevelinheritance/vehicle/Car.java
@@ -0,0 +1,12 @@
+package dayfive.multilevelinheritance.vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
