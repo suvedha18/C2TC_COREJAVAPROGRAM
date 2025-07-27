7df315ac8c92ed6c528cd8abbe97ff173992c05e
HP <HP@LAPTOP-463TH5EG>
2025-07-27 17:18:29 +0530

    This is assignment 3

---
+++ C2TC_COREJAVAPROGRAM/src/Assignment3/Bank.java
@@ -0,0 +1,15 @@
+package Assignment3;
+
+public class Bank {
+	 private static int totalAccounts = 0;
+
+	    public static void incrementAccountCount() {
+	        totalAccounts++;
+	    }
+
+	    public static int getTotalAccounts() {
+	        return totalAccounts;
+	    }
+	
+
+}
